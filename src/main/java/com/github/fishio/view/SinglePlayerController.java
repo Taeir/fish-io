b5ba9ec0062031de2cc2fdda594a7485ff4241e1
Chiel Bruin <chielBruin@tele2.nl>
2015-09-03 19:37:11 +0200

    Added a menu and main game screen
    The menu has two working buttons (single player and exit) and seven disabled buttons for future features.
    The main game now only shows that you died, you can return to the menu or restart the game(shows an empty playing field).
    
    <feature

---
+++ src/main/java/com/github/fishio/view/SinglePlayerController.java
@@ -0,0 +1,56 @@
+package com.github.fishio.view;
+
+import com.github.fishio.FishIO;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+/**
+ * The controller class of the single player game.
+ * 
+ * @author Chiel Bruin
+ * @since 03-09-2015
+ */
+public class SinglePlayerController implements ScreenController {
+
+	private FishIO mainApp;
+	
+	@FXML
+	private VBox deathScreen;
+	
+	@Override
+	public void setMainApp(FishIO mainApp) {
+		this.mainApp = mainApp;
+		showDeathScreen(true);
+	}
+	
+	/**
+	 * Set the visibility of the death screen.
+	 * 
+	 * @param visible
+	 * 			Boolean to indicate if the screen is visible.
+	 */
+	public void showDeathScreen(boolean visible) {
+		if (deathScreen.isVisible() != visible) {
+			deathScreen.setVisible(visible);
+		}
+	}
+	
+	/**
+	 * Opens the main menu.
+	 */
+	@FXML
+	public void backToMenu() {
+		mainApp.openMainMenu();
+	}
+	
+	/**
+	 * Restarts the game.
+	 */
+	@FXML
+	public void restartGame() {
+		showDeathScreen(false);
+		//TODO - reset the map etc.
+	}
+	
+}
