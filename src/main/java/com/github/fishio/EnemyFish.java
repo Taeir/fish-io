315103177e7a20328e1775f18689a9de6cc2de96
Robin van der Wal <apocalytica8@hotmail.com>
2015-09-05 10:47:57 +0200

    Enemy fish collide class.
    > Organisation

---
+++ src/main/java/com/github/fishio/EnemyFish.java
@@ -0,0 +1,19 @@
+package com.github.fishio;
+
+/**
+ * Enemy Fish class.
+ * This class contains all methods concerning enemy fish on the screen.
+ */
+public class EnemyFish extends Entity {
+
+	/**
+	 * Main constructor of enemy fish.
+	 * @param bb Bounding box of enemy fish.
+	 */
+	public EnemyFish(BoundingBox bb) {
+		super(bb);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
