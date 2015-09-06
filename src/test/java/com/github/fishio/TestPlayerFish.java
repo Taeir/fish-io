55b320f62eb586ef0a91f8ca0ff06eb27428e9da
Luka Miljak <lmiljak@student.tudelft.nl>
2015-09-06 14:35:52 +0200

    Test for onCollide for PlayerFish

--- src/test/java/com/github/fishio/TestPlayerFish.java
+++ src/test/java/com/github/fishio/TestPlayerFish.java
@@ -2,6 +2,9 @@ package com.github.fishio;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.when;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import org.junit.Before;
@@ -22,6 +25,7 @@ public class TestPlayerFish {
 	@Before
 	public void setUp() {
 		pf = Mockito.spy(new PlayerFish(Mockito.mock(BoundingBox.class), Mockito.mock(Stage.class)));
+		when(pf.getBoundingBox().getSize()).thenReturn(5.0);
 	}
 	
 	/**
@@ -62,5 +66,69 @@ public class TestPlayerFish {
 	@Test
 	public void testCanMoveThroughWall() {
 		assertFalse(pf.canMoveThroughWall());
+	}
+	
+	/**
+	 * Tests {@link PlayerFish#canMoveThroughWall()}
+	 * using another PlayerFish to collide with.
+	 */
+	@Test
+	public void testCollideWithOtherPlayerFish() {
+		PlayerFish pf2 = Mockito.spy(new PlayerFish(Mockito.mock(BoundingBox.class), Mockito.mock(Stage.class)));
+		
+		pf.onCollide(pf2);
+		
+		Mockito.verify(pf, never()).setDead();
+		Mockito.verify(pf2, never()).setDead();
+	}
+	
+	/**
+	 * Tests {@link PlayerFish#canMoveThroughWall()}
+	 * using a larger EnemyFish to collide with.
+	 */
+	@Test
+	public void testCollideWithLargerEnemyFish() {
+		EnemyFish ef = Mockito.spy(new EnemyFish(Mockito.mock(BoundingBox.class), 
+				Color.ALICEBLUE, 0.0, 0.0));
+		when(ef.getBoundingBox().getSize()).thenReturn(5.1);
+		
+		pf.onCollide(ef);
+		
+		Mockito.verify(pf).setDead();
+		Mockito.verify(ef, never()).setDead();
+	}
+	
+	/**
+	 * Tests {@link PlayerFish#canMoveThroughWall()}
+	 * using a smaller EnemyFish to collide with.
+	 */
+	@Test
+	public void testCollideWithSmallerEnemyFish() {
+		EnemyFish ef = Mockito.spy(new EnemyFish(Mockito.mock(BoundingBox.class), 
+				Color.ALICEBLUE, 0.0, 0.0));
+		when(ef.getBoundingBox().getSize()).thenReturn(4.9);
+		BoundingBox bb = pf.getBoundingBox();
+		
+		pf.onCollide(ef);
+		
+		Mockito.verify(ef).setDead();
+		Mockito.verify(pf, never()).setDead();
+		Mockito.verify(bb).increaseSize(Math.pow(4.9, 0.9));
+	}
+	
+	/**
+	 * Tests {@link PlayerFish#canMoveThroughWall()}
+	 * using a same sized EnemyFish to collide with.
+	 */
+	@Test
+	public void testCollideWithSameSizeEnemyFish() {
+		EnemyFish ef = Mockito.spy(new EnemyFish(Mockito.mock(BoundingBox.class), 
+				Color.ALICEBLUE, 0.0, 0.0));
+		when(ef.getBoundingBox().getSize()).thenReturn(5.0);
+
+		pf.onCollide(ef);
+		
+		Mockito.verify(ef, never()).setDead();
+		Mockito.verify(pf, never()).setDead();
 	}
 }
