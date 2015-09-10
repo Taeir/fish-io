e77da9620c7838867d6ee1b8e44e42ac0c5f5e64
Taico Aerts <info@taico.nl>
2015-09-11 01:16:05 +0200

    Added an extra skip splash test
    Fixed other skip splash test using the wrong key.

--- src/test/java/com/github/fishio/gui/TestSplashScreen.java
+++ src/test/java/com/github/fishio/gui/TestSplashScreen.java
@@ -46,7 +46,28 @@ public class TestSplashScreen extends GuiTest {
 		assertTrue(isCurrentScene(getSplashScene()));
 		
 		//Skip the splash screen
-		press(KeyCode.SPACE);
+		press(KeyCode.K);
+		
+		//Now the main menu should be shown.
+		assertTrue(isCurrentScene(getMainMenuScene()));
+	}
+	
+	/**
+	 * Test for the following scenario, with key ENTER.<br>
+	 * <br>
+	 * Scenario S1.3: Splash Skip 3<br>
+	 * Given the user has launched the Fish.IO GUI,<br>
+	 * and   the splash screen is shown;<br>
+	 * When  the user presses any key;<br>
+	 * Then  the main menu screen should be shown.
+	 */
+	@Test
+	public void testSkipSplash3() {
+		//The splash screen should be shown.
+		assertTrue(isCurrentScene(getSplashScene()));
+		
+		//Skip the splash screen
+		press(KeyCode.ENTER);
 		
 		//Now the main menu should be shown.
 		assertTrue(isCurrentScene(getMainMenuScene()));
@@ -55,7 +76,7 @@ public class TestSplashScreen extends GuiTest {
 	/**
 	 * Test for the following scenario:<br>
 	 * <br>
-	 * Scenario S1.3: Wait for Splash<br>
+	 * Scenario S1.4: Wait for Splash<br>
 	 * Given the user has launched the Fish.IO GUI,<br>
 	 * and   the splash screen is shown;<br>
 	 * When  the user waits for 15 seconds;<br>
