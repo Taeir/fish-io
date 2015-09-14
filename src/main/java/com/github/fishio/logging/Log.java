431b90df7b20e476fcbc7b4e7b7ad1cb8b421a1e
Robin van der Wal <apocalytica8@hotmail.com>
2015-09-14 15:20:40 +0200

    Added a new logging package and logging class.
    > feature

---
+++ src/main/java/com/github/fishio/logging/Log.java
@@ -0,0 +1,11 @@
+package com.github.fishio.logging;
+
+/**
+ * The logging class.
+ * This class can be called from other classes to log events. The
+ * output of this class is then send to a handler which handles the 
+ * log and deposits it in the appropriate places.
+ */
+public final class Log {
+
+}
