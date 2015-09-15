b1abb4d89ade7c861c582f2ea9e8d443c79ecae8
Robin van der Wal <apocalytica8@hotmail.com>
2015-09-15 08:43:29 +0200

    Minor add.
    > feature

--- src/main/java/com/github/fishio/logging/Log.java
+++ src/main/java/com/github/fishio/logging/Log.java
@@ -6,7 +6,20 @@ package com.github.fishio.logging;
  * output of this class is then send to a handler which handles the 
  * log and deposits it in the appropriate places.
  */
-public final class Log {
+public class Log {
 
+	private static final Log log = new Log();
 	
+	/**
+	 * Private constructor to prevent initialization,
+	 */
+	private Log() {}
+	
+	/**
+	 * Return logger instance.
+	 * @return logging
+	 */
+	public static Log getLogger() {
+		return log;
+	}
 }
