5a0cfe0671d04b6be3cbe40283b5700c700d5b90
Robin van der Wal <apocalytica8@hotmail.com>
2015-09-22 10:08:09 +0200

    Added factory testing method.

--- src/test/java/com/github/fishio/logging/TestIHandler.java
+++ src/test/java/com/github/fishio/logging/TestIHandler.java
@@ -1,8 +1,10 @@
 package com.github.fishio.logging;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
-
-
+import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * The class that should test methods that are the same in all implementations. 
@@ -10,6 +12,18 @@ package com.github.fishio.logging;
  */
 public abstract class TestIHandler {
 
-	//TODO Not sure how to implement factory method tests yet
+	private IHandler handler;
+	
+	/**
+	 * Test the set and get method for the formatter.
+	 */
+	@Test
+	public void testGetSetFormatter() {
+		IFormatter formatter = Mockito.mock(DefaultFormat.class);
+		when(formatter.formatOutput(LogLevel.ERROR, "Test")).thenReturn("Test Output");
+		
+		handler.setFormat(formatter);
+		assertEquals(formatter, handler.getFormat());
+	}
 
 }
