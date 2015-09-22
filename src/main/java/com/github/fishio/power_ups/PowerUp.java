bd0ce4cb1a8d47d2d2259fe0eba5451724dcb435
Luka Miljak <lmiljak@student.tudelft.nl>
2015-09-22 10:36:13 +0200

    Updated the PowerUp class for its future children
    Splitsed the abstract onCollide method into a non-abstract onCollide
    method and an abstract executeEffect(PlayerFish pfish). The executeEffect
    method is automatically called when the other:ICollidable in the onCollide
    method is a PlayerFish.
    
    Added the field pfield : PlayingField to PowerUp, which can be accesed
    using getPField(). This is useful for the child classes of PowerUp when
    they implement the executeEffect method.
    
    <organisation

--- src/main/java/com/github/fishio/power_ups/PowerUp.java
+++ src/main/java/com/github/fishio/power_ups/PowerUp.java
@@ -3,24 +3,53 @@ package com.github.fishio.power_ups;
 import com.github.fishio.Entity;
 import com.github.fishio.ICollidable;
 import com.github.fishio.ICollisionArea;
+import com.github.fishio.PlayerFish;
+import com.github.fishio.PlayingField;
 
 /**
- * A Power-Up is an entity that, when colliding with a player fish,
+ * A PowerUp is an entity that, when colliding with a player fish,
  * executes a certain (positive) effect. 
  */
 public abstract class PowerUp extends Entity {
 
+	private PlayingField pfield;
+	
 	/**
-	 * Creates a new Power-Up.
+	 * Creates a new PowerUp.
 	 * 
 	 * @param ba
 	 * 		The CollisionArea of the Power-Up
+	 * @param pfield
+	 * 		The PlayingField this PowerUp is located in
 	 */
-	public PowerUp(ICollisionArea ba) {
+	public PowerUp(ICollisionArea ba, PlayingField pfield) {
 		super(ba);
+		
+		this.pfield = pfield;
 	}
 	
+	/**
+	 * @return
+	 * 		The PlayingField this PowerUp is located in.
+	 */
+	public PlayingField getPField() {
+		return pfield;
+	}
+	
+	/**
+	 * Executes the effect the power-up should do
+	 * when a collision happens with a PlayerFish.
+	 * 
+	 * @param pfish
+	 * 		The PlayerFish this PowerUp collided with
+	 */
+	public abstract void executeEffect(PlayerFish pfish);
+	
 	@Override
-	public abstract void onCollide(ICollidable other);
+	public void onCollide(ICollidable other) {
+		if (other instanceof PlayerFish) {
+			executeEffect((PlayerFish) other);
+		}
+	}
 	
 }
