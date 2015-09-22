30c59f76aa70871434914edf87363e6f348069b3
Luka Miljak <lmiljak@student.tudelft.nl>
2015-09-22 11:29:23 +0200

    Added a PowerUp that freezes all EnemyFish.
    When this PowerUp is picked up by the PlayerFish, all EnemyFishes
    in the PlayingField freeze immediately. After 10 seconds the
    EnemyFishes that are still frozen are unfrozen again.

---
+++ src/main/java/com/github/fishio/power_ups/PuFreeze.java
@@ -0,0 +1,62 @@
+package com.github.fishio.power_ups;
+
+import com.github.fishio.EnemyFish;
+import com.github.fishio.Entity;
+import com.github.fishio.ICollisionArea;
+import com.github.fishio.PlayerFish;
+import com.github.fishio.PlayingField;
+import com.github.fishio.listeners.TickListener;
+
+/**
+ * A PowerUp with the effect that it freezes all
+ * current EnemyFishes in the PlayingField for 10 seconds. 
+ */
+public class PuFreeze extends DurationPowerUp {
+
+	/**
+	 * The duration of the effect in seconds.
+	 */
+	private static final int DURATION = 10;
+	
+	/**
+	 * Creates a new PowerUp of the Freeze type.
+	 * 
+	 * @param ba
+	 * 		The CollisonArea of the PowerUp.
+	 * @param pfield
+	 * 		The PlayingField in which this PowerUp is located.
+	 */
+	public PuFreeze(ICollisionArea ba, PlayingField pfield) {
+		super(ba, pfield);
+	}
+
+	@Override
+	public int getDuration() {
+		return DURATION;
+	}
+
+	@Override
+	public void startEffect(PlayerFish pf) {
+		for (Entity e : getPField().getEntities()) {
+			if (e instanceof EnemyFish) {
+				((EnemyFish) e).setFrozen(true);
+			}
+		}
+	}
+
+	@Override
+	public void preTickEffect() { }
+
+	@Override
+	public void postTickEffect() { }
+
+	@Override
+	public void endEffect() {
+		for (Entity e : getPField().getEntities()) {
+			if (e instanceof EnemyFish) {
+				((EnemyFish) e).setFrozen(false);
+			}
+		}
+	}
+
+}
