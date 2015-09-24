c5df6f0177672ca598178b9d6a2dd15343267868
Luka Miljak <lmiljak@student.tudelft.nl>
2015-09-24 12:27:27 +0200

    Added a PowerUp class that increases lives.
    PuExtraLife has the effect that it instantly increments the amount
    of lives of the PlayerFish.
    
    <feature

---
+++ src/main/java/com/github/fishio/power_ups/PuExtraLife.java
@@ -0,0 +1,30 @@
+package com.github.fishio.power_ups;
+
+import com.github.fishio.ICollisionArea;
+import com.github.fishio.PlayerFish;
+import com.github.fishio.PlayingField;
+
+/**
+ * A PowerUp with the effect that it instantly increases
+ * the PlayerFish' life (if it isn't maxed out already).
+ */
+public class PuExtraLife extends PowerUp {
+
+	/**
+	 * Creates a new PowerUp of the Extra Life type.
+	 * 
+	 * @param ba
+	 * 		The CollisonArea of the PowerUp.
+	 * @param pfield
+	 * 		The PlayingField in which this PowerUp is located.
+	 */
+	public PuExtraLife(ICollisionArea ba, PlayingField pfield) {
+		super(ba, pfield);
+	}
+
+	@Override
+	public void executeEffect(PlayerFish pfish) {
+		pfish.addLife();
+	}
+
+}
