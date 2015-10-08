a871f6153713e0d899389f9db4f9d21066bed780
jessearens <jesse.arens@hotmail.com>
2015-10-08 17:36:41 +0200

    Remove commented code.
    <feature,layout

--- src/main/java/com/github/fishio/control/AchievementScreenController.java
+++ src/main/java/com/github/fishio/control/AchievementScreenController.java
@@ -1,8 +1,5 @@
 package com.github.fishio.control;
 
-
-
-
 import com.github.fishio.Preloader;
 import com.github.fishio.achievements.Achieve;
 import com.github.fishio.achievements.Achievement;
@@ -45,57 +42,6 @@ public class AchievementScreenController implements ScreenController {
 	private Label smallachieve24;
 	@FXML
 	private Label smallachieve25;
-	/*
-	 * @FXML private Label smallachieve31;
-	 * 
-	 * @FXML private Label smallachieve32;
-	 * 
-	 * @FXML private Label smallachieve33;
-	 * 
-	 * @FXML private Label smallachieve34;
-	 * 
-	 * @FXML private Label smallachieve35;
-	 * 
-	 * @FXML private Label smallachieve41;
-	 * 
-	 * @FXML private Label smallachieve42;
-	 * 
-	 * @FXML private Label smallachieve43;
-	 * 
-	 * @FXML private Label smallachieve44;
-	 * 
-	 * @FXML private Label smallachieve45;
-	 * 
-	 * @FXML private Label smallachieve51;
-	 * 
-	 * @FXML private Label smallachieve52;
-	 * 
-	 * @FXML private Label smallachieve53;
-	 * 
-	 * @FXML private Label smallachieve54;
-	 * 
-	 * @FXML private Label smallachieve55;
-	 * 
-	 * @FXML private Label smallachieve61;
-	 * 
-	 * @FXML private Label smallachieve62;
-	 * 
-	 * @FXML private Label smallachieve63;
-	 * 
-	 * @FXML private Label smallachieve64;
-	 * 
-	 * @FXML private Label smallachieve65;
-	 * 
-	 * @FXML private Label smallachieve71;
-	 * 
-	 * @FXML private Label smallachieve72;
-	 * 
-	 * @FXML private Label smallachieve73;
-	 * 
-	 * @FXML private Label smallachieve74;
-	 * 
-	 * @FXML private Label smallachieve75;
-	 */
 	@FXML
 	private Label icon11;
 	@FXML
@@ -117,58 +63,6 @@ public class AchievementScreenController implements ScreenController {
 	@FXML
 	private Label icon25;
 	
-	/*
-	 * @FXML private Label icon31;
-	 * 
-	 * @FXML private Label icon32;
-	 * 
-	 * @FXML private Label icon33;
-	 * 
-	 * @FXML private Label icon34;
-	 * 
-	 * @FXML private Label icon35;
-	 * 
-	 * @FXML private Label icon41;
-	 * 
-	 * @FXML private Label icon42;
-	 * 
-	 * @FXML private Label icon43;
-	 * 
-	 * @FXML private Label icon44;
-	 * 
-	 * @FXML private Label icon45;
-	 * 
-	 * @FXML private Label icon51;
-	 * 
-	 * @FXML private Label icon52;
-	 * 
-	 * @FXML private Label icon53;
-	 * 
-	 * @FXML private Label icon54;
-	 * 
-	 * @FXML private Label icon55;
-	 * 
-	 * @FXML private Label icon61;
-	 * 
-	 * @FXML private Label icon62;
-	 * 
-	 * @FXML private Label icon63;
-	 * 
-	 * @FXML private Label icon64;
-	 * 
-	 * @FXML private Label icon65;
-	 * 
-	 * @FXML private Label icon71;
-	 * 
-	 * @FXML private Label icon72;
-	 * 
-	 * @FXML private Label icon73;
-	 * 
-	 * @FXML private Label icon74;
-	 * 
-	 * @FXML private Label icon75;
-	 * 
-	 */
 	/**
 	 * Controls the viewer for the first achievement: player deaths.
 	 */
@@ -252,33 +146,6 @@ public class AchievementScreenController implements ScreenController {
 		icon23.opacityProperty().bind(smallachieve23.opacityProperty());
 		icon24.opacityProperty().bind(smallachieve24.opacityProperty());
 		icon25.opacityProperty().bind(smallachieve25.opacityProperty());
-		/*
-		 * icon31.opacityProperty().bind(smallachieve31.opacityProperty());
-		 * icon32.opacityProperty().bind(smallachieve32.opacityProperty());
-		 * icon33.opacityProperty().bind(smallachieve33.opacityProperty());
-		 * icon34.opacityProperty().bind(smallachieve34.opacityProperty());
-		 * icon35.opacityProperty().bind(smallachieve35.opacityProperty());
-		 * icon41.opacityProperty().bind(smallachieve41.opacityProperty());
-		 * icon42.opacityProperty().bind(smallachieve42.opacityProperty());
-		 * icon43.opacityProperty().bind(smallachieve43.opacityProperty());
-		 * icon44.opacityProperty().bind(smallachieve44.opacityProperty());
-		 * icon45.opacityProperty().bind(smallachieve45.opacityProperty());
-		 * icon51.opacityProperty().bind(smallachieve51.opacityProperty());
-		 * icon52.opacityProperty().bind(smallachieve52.opacityProperty());
-		 * icon53.opacityProperty().bind(smallachieve53.opacityProperty());
-		 * icon54.opacityProperty().bind(smallachieve54.opacityProperty());
-		 * icon55.opacityProperty().bind(smallachieve55.opacityProperty());
-		 * icon61.opacityProperty().bind(smallachieve61.opacityProperty());
-		 * icon62.opacityProperty().bind(smallachieve62.opacityProperty());
-		 * icon63.opacityProperty().bind(smallachieve63.opacityProperty());
-		 * icon64.opacityProperty().bind(smallachieve64.opacityProperty());
-		 * icon65.opacityProperty().bind(smallachieve65.opacityProperty());
-		 * icon71.opacityProperty().bind(smallachieve71.opacityProperty());
-		 * icon72.opacityProperty().bind(smallachieve72.opacityProperty());
-		 * icon73.opacityProperty().bind(smallachieve73.opacityProperty());
-		 * icon74.opacityProperty().bind(smallachieve74.opacityProperty());
-		 * icon75.opacityProperty().bind(smallachieve75.opacityProperty());
-		 */
 		smallachieve11.setOpacity(0.1);
 		smallachieve12.setOpacity(0.1);
 		smallachieve13.setOpacity(0.1);
@@ -289,21 +156,6 @@ public class AchievementScreenController implements ScreenController {
 		smallachieve23.setOpacity(0.1);
 		smallachieve24.setOpacity(0.1);
 		smallachieve25.setOpacity(0.1);
-		/*
-		 * smallachieve31.setOpacity(0.1); smallachieve32.setOpacity(0.1);
-		 * smallachieve33.setOpacity(0.1); smallachieve34.setOpacity(0.1);
-		 * smallachieve35.setOpacity(0.1); smallachieve41.setOpacity(0.1);
-		 * smallachieve42.setOpacity(0.1); smallachieve43.setOpacity(0.1);
-		 * smallachieve44.setOpacity(0.1); smallachieve45.setOpacity(0.1);
-		 * smallachieve51.setOpacity(0.1); smallachieve52.setOpacity(0.1);
-		 * smallachieve53.setOpacity(0.1); smallachieve54.setOpacity(0.1);
-		 * smallachieve55.setOpacity(0.1); smallachieve61.setOpacity(0.1);
-		 * smallachieve62.setOpacity(0.1); smallachieve63.setOpacity(0.1);
-		 * smallachieve64.setOpacity(0.1); smallachieve65.setOpacity(0.1);
-		 * smallachieve71.setOpacity(0.1); smallachieve72.setOpacity(0.1);
-		 * smallachieve73.setOpacity(0.1); smallachieve74.setOpacity(0.1);
-		 * smallachieve75.setOpacity(0.1);
-		 */
 	}
 	
 	@Override
