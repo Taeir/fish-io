807c96ade515fcb4ebe22f26bf0ab690442830fa
Taico Aerts <info@taico.nl>
2015-10-16 01:46:29 +0200

    Added serverside multiplayer implementation
    <feature

---
+++ src/main/java/com/github/fishio/multiplayer/MultiplayerPlayingField.java
@@ -0,0 +1,53 @@
+package com.github.fishio.multiplayer;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javafx.scene.canvas.Canvas;
+
+import com.github.fishio.PlayerFish;
+import com.github.fishio.PlayingField;
+
+/**
+ * PlayingField for a multiplayer game.
+ */
+public abstract class MultiplayerPlayingField extends PlayingField {
+	private Set<PlayerFish> players = Collections.newSetFromMap(new ConcurrentHashMap<PlayerFish, Boolean>());
+	
+	/**
+	 * Creates a new MultiplayerPlayingField.
+	 * 
+	 * @param fps
+	 * 		the fps of the renderer of this PlayingField.
+	 * @param canvas
+	 * 		the canvas to render on.
+	 */
+	public MultiplayerPlayingField(int fps, Canvas canvas) {
+		super(fps, canvas);
+		
+	}
+
+	@Override
+	public Set<PlayerFish> getPlayers() {
+		return players;
+	}
+	
+	@Override
+	public void add(Object o) {
+		super.add(o);
+		
+		if (o instanceof PlayerFish) {
+			players.add((PlayerFish) o);
+		}
+	}
+	
+	@Override
+	public void remove(Object o) {
+		super.remove(o);
+		
+		if (o instanceof PlayerFish) {
+			players.remove(o);
+		}
+	}
+}
