4f03ac2720dc7e22a8a5df2ffe7d8a6d1483e8ff
Taico Aerts <info@taico.nl>
2015-10-16 16:25:29 +0200

    Link the multiplayerScreen
    <user-interface

--- src/main/java/com/github/fishio/control/MultiplayerScreenController.java
+++ src/main/java/com/github/fishio/control/MultiplayerScreenController.java
@@ -1,10 +1,19 @@
 package com.github.fishio.control;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.github.fishio.Preloader;
+import com.github.fishio.Util;
 import com.github.fishio.audio.AudioEngine;
+import com.github.fishio.game.GameState;
+import com.github.fishio.game.GameThread;
 import com.github.fishio.logging.Log;
 import com.github.fishio.logging.LogLevel;
+import com.github.fishio.multiplayer.client.FishIOClient;
+import com.github.fishio.multiplayer.server.FishIOServer;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -17,6 +26,7 @@ import javafx.scene.control.TextField;
 public class MultiplayerScreenController implements ScreenController {
 
 	private Log logger = Log.getLogger();
+	private Pattern addressPattern = Pattern.compile("(?<address>.*)(?<port>:\\d+)?");
 	
 	@FXML
 	private Button btnBackToMenu;
@@ -37,7 +47,19 @@ public class MultiplayerScreenController implements ScreenController {
 	private Canvas keyboard;
 	
 	@Override
-	public void init(Scene scene) { }
+	public void init(Scene scene) {
+		ChangeListener<? super GameState> cl = (o, oVal, nVal) -> {
+			if (nVal == GameState.RUNNING) {
+				Util.onJavaFX(() -> Preloader.switchTo("multiplayerGameScreen", 0));
+			}
+		};
+		
+		GameThread gt = FishIOClient.getInstance().getPlayingField().getGameThread();
+		gt.stateProperty().addListener(cl);
+		
+		gt = FishIOServer.getInstance().getPlayingField().getGameThread();
+		gt.stateProperty().addListener(cl);
+	}
 
 	@Override
 	public void onSwitchTo() { }
@@ -60,7 +82,28 @@ public class MultiplayerScreenController implements ScreenController {
 		AudioEngine.getInstance().playEffect("button");
 		logger.log(LogLevel.INFO, "Player Pressed JoinGame Button.");
 
-		//TODO Actually join a multiplayer game
+		String s = joinIP.getText();
+		Matcher m = addressPattern.matcher(s);
+		
+		//Invalid pattern
+		if (!m.matches()) {
+			joinIP.setStyle("-fx-text-color: red");
+			return;
+		}
+		
+		//Reset style
+		joinIP.setStyle("");
+		
+		String sport = m.group("port");
+		int port;
+		if (sport == null || sport.isEmpty()) {
+			port = 25565;
+		} else {
+			port = Integer.parseInt(sport);
+		}
+		
+		//Start connecting
+		FishIOClient.getInstance().connect(m.group("address"), port);
 	}
 	
 	/**
@@ -71,7 +114,21 @@ public class MultiplayerScreenController implements ScreenController {
 		AudioEngine.getInstance().playEffect("button");
 		logger.log(LogLevel.INFO, "Player Pressed HostGame Button.");
 
-		//TODO Actually start a multiplayer game
+		String s = createGamePort.getText();
+		
+		//Invalid port
+		if (!s.matches("\\d+")) {
+			createGamePort.setStyle("-fx-text-color: red");
+			return;
+		}
+		
+		//Reset style
+		createGamePort.setStyle("");
+		
+		int port = Integer.parseInt(s);
+		
+		//Start the server
+		FishIOServer.getInstance().start(port);
 	}
 
 }
