b69194cd64b843149a12fbc0a381c2e3bdbf3266
Taico Aerts <info@taico.nl>
2015-10-17 18:05:36 +0200

    Added Sprites + SpriteStore
    This functions as a bit of a replacement for the Preloader with collision
    mask data.
    
    <feature

---
+++ src/main/java/com/github/fishio/Sprite.java
@@ -0,0 +1,83 @@
+package com.github.fishio;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+
+/**
+ * Image with collision mask data.
+ */
+public class Sprite extends Image {
+	private double alphaRatio;
+	private boolean[][] pixelData;
+	
+	/**
+	 * Constructs a MaskedImage from the file at the given url.
+	 * 
+	 * @param url
+	 * 		the string representing the URL to use in fetching the pixel data
+	 */
+	public Sprite(String url) {
+		super(url);
+		
+		this.pixelData = buildPixelData();
+		this.alphaRatio = calculateAlphaRatio();
+	}
+	
+	/**
+	 * Build the data used for the collision mask.
+	 * 
+	 * @return
+	 * 		the generated pixel data matrix.
+	 */
+	private boolean[][] buildPixelData() {
+		int width = (int) getWidth();
+		int heigth = (int) getHeight();
+		
+		boolean[][] res = new boolean[width][heigth];
+		
+		PixelReader pr = getPixelReader();
+		for (int y = 0; y < heigth; y++) {
+			for (int x = 0; x < width; x++) {
+				res[x][y] = pr.getColor(x, y).getOpacity() > 0.5;
+			}
+		}
+		
+		return res;
+	}
+	
+	/**
+	 * Calculates the alpha ration of this image.
+	 * 
+	 * @return
+	 * 		the ratio between opaque and non-opaque pixels.
+	 */
+	private double calculateAlphaRatio() {
+		int res = 0;
+		for (boolean[] row : pixelData) {
+			for (boolean pixel : row) {
+				if (pixel) {
+					res++;
+				}
+			}
+		}
+
+		return ((double) res) / (pixelData.length * pixelData[0].length);
+	}
+	
+	/**
+	 * @return
+	 * 		the alpha ration of this image.
+	 */
+	public double getAlphaRatio() {
+		return this.alphaRatio;
+	}
+	
+	/**
+	 * @return
+	 * 		a boolean matrix with true for pixels that are part of the mask,
+	 * 		and false for all pixels that are not.
+	 */
+	public boolean[][] getPixelData() {
+		return this.pixelData;
+	}
+}
