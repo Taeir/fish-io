4d341ac61afda7deff545e0c05a11195448a6ce5
Luka Miljak <lmiljak@student.tudelft.nl>
2015-10-27 10:46:36 +0100

    Added a setUp method for TestIDrawable
    <testing

--- src/test/java/com/github/fishio/TestIDrawableDebugDraw.java
+++ src/test/java/com/github/fishio/TestIDrawableDebugDraw.java
@@ -1,8 +1,26 @@
 package com.github.fishio;
 
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 /**
  * Test class for the debugDraw in IDrawable.
  */
 public class TestIDrawableDebugDraw {
+	
+	private IDrawable drawable;
+	private CollisionMask collisionArea;
+	private GraphicsContext graphicsContext;
+	
+	@Before
+	public void setUp() {
+		this.collisionArea = Mockito.mock(CollisionMask.class);
+		this.drawable = new EnemyFish(collisionArea, null, 0, 0);
+		
+		graphicsContext = Mockito.spy((new Canvas()).getGraphicsContext2D());
+	}
 	
 }
