ee982a48af22c529cb69f420a3015294405cc017
Chiel Bruin <chielBruin@tele2.nl>
2015-10-27 11:34:27 +0100

    Added javadoc
    <documentation

--- src/test/java/com/github/fishio/TestPreloader.java
+++ src/test/java/com/github/fishio/TestPreloader.java
@@ -21,11 +21,17 @@ import com.github.fishio.logging.Log;
 import com.github.fishio.logging.LogLevel;
 import com.github.fishio.settings.Settings;
 
-public class TestPreloader extends SlimGuiTest{
+/**
+ * Test for the {@link Preloader} class.
+ */
+public class TestPreloader extends SlimGuiTest {
 	private static HashMap<String, Image> images;
 	private static HashMap<String, Scene> screens;
 	private ConsoleHandler consoleHandler;
 	
+	/**
+	 * Set the logLevel to trace and get the hashMaps containing loaded images and screens.
+	 */
 	@BeforeClass
 	public static void beforeClass() {
 		Log.getLogger().setLogLevel(LogLevel.TRACE);
@@ -33,11 +39,17 @@ public class TestPreloader extends SlimGuiTest{
 		screens = Preloader.getScreens();
 	}
 	
+	/**
+	 * Restore the logLoevel after the tests.
+	 */
 	@AfterClass
 	public static void afterClass() {
 		Log.getLogger().setLogLevel(LogLevel.fromInt(Settings.getInstance().getInteger("LOG_LEVEL")));
 	}
 	
+	/**
+	 * Before each test reset the consoleHandler and clear all loaded images and screens.
+	 */
 	@Before
 	public  void before() {
 		Log log = Log.getLogger();
@@ -49,18 +61,27 @@ public class TestPreloader extends SlimGuiTest{
 		screens.clear();
 	}
 	
+	/**
+	 * Test if preloading is logged.
+	 */
 	@Test
 	public void testPreloadImagesLog() {
 		Preloader.preloadImages();
 		verify(consoleHandler).output(LogLevel.DEBUG, "[Preloader] Preloading images...");
 	}
 	
+	/**
+	 * Test the exception handling for loading a faulty image.
+	 */
 	@Test
 	public void testTryLoadImagesException() {
 		Preloader.tryPreLoad("banana");
 		verify(consoleHandler).output(LogLevel.ERROR, "Error while trying to load image: banana");
 	}
 	
+	/**
+	 * Test if loading an exist.
+	 */
 	@Test
 	public void testTryLoadImagesExisting() {
 		Image image = new Image("AlphaDataTest.png");
@@ -69,6 +90,9 @@ public class TestPreloader extends SlimGuiTest{
 		assertEquals(image, images.get("banana")); // not overwritten		
 	}
 	
+	/**
+	 * Test the loading of an image.
+	 */
 	@Test
 	public void testTryLoadImagesLoad() {
 		Preloader.tryPreLoad("AlphaDataTest.png");
@@ -76,6 +100,9 @@ public class TestPreloader extends SlimGuiTest{
 		assertEquals(3, images.get("AlphaDataTest.png").getWidth(), 1E-8);
 	}
 	
+	/**
+	 * Test getting an existing image.
+	 */
 	@Test
 	public void testGetImageOrLoadGet() {
 		Image image = new Image("AlphaDataTest.png");
@@ -83,12 +110,18 @@ public class TestPreloader extends SlimGuiTest{
 		assertEquals(image, Preloader.getImageOrLoad("banana"));	
 	}
 	
+	/**
+	 * Test the loading of a non existing image.
+	 */
 	@Test
 	public void testGetImageOrLoadLoad() {
 		Image image = Preloader.getImageOrLoad("AlphaDataTest.png");
 		assertEquals(3, image.getWidth(), 1E-8);
 	}
 	
+	/**
+	 * Get an existing image and check if it returns the correct image.
+	 */
 	@Test
 	public void testGetImageExisting() {
 		Image image = new Image("AlphaDataTest.png");
@@ -97,7 +130,10 @@ public class TestPreloader extends SlimGuiTest{
 		assertEquals(image, images.get("banana"));		
 	}
 	
-	@Test(expected=IllegalArgumentException.class)
+	/**
+	 * Get not loaded image and check the logging and exception throwing.
+	 */
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetImageLoad() {
 		Preloader.getImage("banana");
 		verify(consoleHandler).output(LogLevel.ERROR, "No image loaded for banana!");
