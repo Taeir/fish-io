993e972c9cf3218538b45bb143a1f51a7b0f0b27
Luka Miljak <lmiljak@student.tudelft.nl>
2015-10-27 11:46:01 +0100

    Added a static import
    <layout

--- src/test/java/com/github/fishio/TestIDrawableDebugDraw.java
+++ src/test/java/com/github/fishio/TestIDrawableDebugDraw.java
@@ -1,12 +1,15 @@
 package com.github.fishio;
 
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import javafx.scene.canvas.GraphicsContext;
@@ -34,23 +37,23 @@ public class TestIDrawableDebugDraw {
 		this.collisionArea = Mockito.mock(CollisionMask.class);
 		this.drawable = new EnemyFish(collisionArea, null, 0, 0);
 		
-		this.graphicsContext = PowerMockito.mock(GraphicsContext.class);
+		this.graphicsContext = mock(GraphicsContext.class);
 		
-		Mockito.when(collisionArea.getRotation()).thenReturn(42D);
-		Mockito.when(collisionArea.getCenterX()).thenReturn(69D);
-		Mockito.when(collisionArea.getCenterY()).thenReturn(1024D);
-		Mockito.when(collisionArea.getHeight()).thenReturn(22D);
-		Mockito.when(collisionArea.getSize()).thenReturn(33D);
-		Mockito.when(collisionArea.getBottomLeft()).thenReturn(new Vec2d(1, 1));
-		Mockito.when(collisionArea.getBottomRight()).thenReturn(new Vec2d(2, 2));
-		Mockito.when(collisionArea.getTopLeft()).thenReturn(new Vec2d(3, 3));
-		Mockito.when(collisionArea.getTopRight()).thenReturn(new Vec2d(4, 4));
+		when(collisionArea.getRotation()).thenReturn(42D);
+		when(collisionArea.getCenterX()).thenReturn(69D);
+		when(collisionArea.getCenterY()).thenReturn(1024D);
+		when(collisionArea.getHeight()).thenReturn(22D);
+		when(collisionArea.getSize()).thenReturn(33D);
+		when(collisionArea.getBottomLeft()).thenReturn(new Vec2d(1, 1));
+		when(collisionArea.getBottomRight()).thenReturn(new Vec2d(2, 2));
+		when(collisionArea.getTopLeft()).thenReturn(new Vec2d(3, 3));
+		when(collisionArea.getTopRight()).thenReturn(new Vec2d(4, 4));
 		
 		HashSet<Vec2d> set = new HashSet<>();
 		set.add(new Vec2d());
 		set.add(new Vec2d(100,100));
 		set.add(new Vec2d(200,200));
-		Mockito.when(collisionArea.getMask()).thenReturn(set);
+		when(collisionArea.getMask()).thenReturn(set);
 	}
 	
 	/**
@@ -61,22 +64,22 @@ public class TestIDrawableDebugDraw {
 		drawable.debugDraw(graphicsContext, collisionArea);
 		
 		// Verifying the angle and sizes are being drawn in the correct place.
-		Mockito.verify(graphicsContext).fillText("angle: 42.0", 69D, 1003D);
-		Mockito.verify(graphicsContext).fillText("size: 33.0", 69D, 988D);
+		verify(graphicsContext).fillText("angle: 42.0", 69D, 1003D);
+		verify(graphicsContext).fillText("size: 33.0", 69D, 988D);
 		
 		// Verifying the collisionmask is properly being drawn.
-		Mockito.verify(graphicsContext).fillOval(0, 0, 1, 1);
-		Mockito.verify(graphicsContext).fillOval(100, 100, 1, 1);
-		Mockito.verify(graphicsContext).fillOval(200, 200, 1, 1);
+		verify(graphicsContext).fillOval(0, 0, 1, 1);
+		verify(graphicsContext).fillOval(100, 100, 1, 1);
+		verify(graphicsContext).fillOval(200, 200, 1, 1);
 		
 		// Verifying the center is properly being drawn.
-		Mockito.verify(graphicsContext).fillOval(69D, 1024D, 2, 2);
+		verify(graphicsContext).fillOval(69D, 1024D, 2, 2);
 		
 		// Verifying the corners of collisionarea are properly being drawn.
-		Mockito.verify(graphicsContext).fillOval(1D, 1D, 2, 2);
-		Mockito.verify(graphicsContext).fillOval(2D, 2D, 2, 2);
-		Mockito.verify(graphicsContext).fillOval(3D, 3D, 2, 2);
-		Mockito.verify(graphicsContext).fillOval(4D, 4D, 2, 2);
+		verify(graphicsContext).fillOval(1D, 1D, 2, 2);
+		verify(graphicsContext).fillOval(2D, 2D, 2, 2);
+		verify(graphicsContext).fillOval(3D, 3D, 2, 2);
+		verify(graphicsContext).fillOval(4D, 4D, 2, 2);
 	}
 	
 }
